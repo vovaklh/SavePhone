33a8421f62dd7be482cad09cfda52b3299757c2b
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-08-03 19:48:53 +0300

    refactor

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,8 +5,8 @@ import 'package:save_phone/widgets/liquid_button.dart';
 import '../providers/liquid_button_provider.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
   final String title;
+  const HomePage({required this.title, super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -15,38 +15,47 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    TextTheme textTheme = Theme.of(context).textTheme;
+
     return ChangeNotifierProvider(
       create: (context) => LiquidButtonProvider(),
-      child: Consumer<LiquidButtonProvider>(builder: (context, provider, _) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            centerTitle: true,
-            title: Text(widget.title),
-          ),
-          body: Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const LiquidButton(),
-                const SizedBox(height: 60),
-                Text('Signalization is turned ${provider.isOn ? 'ON' : 'OFF'}',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineSmall
-                        ?.copyWith(fontWeight: FontWeight.bold)),
-                provider.isOn
-                    ? Text(
-                        "Your phone is safe. You'll hear an alarm if it's moved",
-                        style: Theme.of(context).textTheme.bodyMedium)
-                    : Text('Tap to prevent theft',
-                        style: Theme.of(context).textTheme.bodyMedium),
-              ],
+      child: Consumer<LiquidButtonProvider>(
+        builder: (context, buttonState, _) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: colorScheme.inversePrimary,
+              centerTitle: true,
+              title: Text(widget.title),
             ),
-          ),
-        );
-      }),
+            body: Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const LiquidButton(),
+                  const SizedBox(height: 60),
+                  Text(
+                    'Signalization is turned ${buttonState.isOn ? 'ON' : 'OFF'}',
+                    style: textTheme.headlineSmall?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  buttonState.isOn
+                      ? Text(
+                          "Your phone is safe. You'll hear an alarm if it's moved",
+                          style: textTheme.bodyMedium,
+                        )
+                      : Text(
+                          'Tap to prevent theft',
+                          style: textTheme.bodyMedium,
+                        ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
