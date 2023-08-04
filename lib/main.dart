77fafca0f02d0b3a33041627f3c565c4b37278a6
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-08-04 17:49:00 +0300

    sound is played on background

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:save_phone/application.dart';
 
-void main() => runApp(const SavePhoneApp());
+void main() async {
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.example.save_phone',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+  );
+  runApp(const SavePhoneApp());
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:audio_session/audio_session.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:save_phone/utils/extensions/build_context_ext.dart';
 import 'package:save_phone/widgets/liquid_button.dart';
 
@@ -12,11 +15,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final AudioPlayer _player = AudioPlayer();
+
+  void _playAlarm() async {
+    final session = await AudioSession.instance;
+    await session.configure(const AudioSessionConfiguration.speech());
+
+    try {
+      await _player.setAudioSource(
+        ClippingAudioSource(
+          start: const Duration(seconds: 60),
+          end: const Duration(seconds: 90),
+          child: AudioSource.uri(Uri.parse(
+              "https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3")),
+          tag: MediaItem(
+            id: '0',
+            album: "Science Friday",
+            title: "A Salute To Head-Scratching Science (30 seconds)",
+            artUri: Uri.parse(
+                "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg"),
+          ),
+        ),
+      );
+    } catch (e) {
+      print("Error loading audio source: $e");
+    }
+    _player.play();
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
       valueListenable: LiquidButtonNotifier.isOn,
       builder: (context, isButtonOn, child) {
+        if (isButtonOn) {
+          _playAlarm();
+        }
         return Scaffold(
           appBar: AppBar(
             backgroundColor: context.colorScheme.inversePrimary,
