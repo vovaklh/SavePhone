f8290d0069c2caf33febfc15b7d6f3e52e5926dc
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-08-16 12:36:10 +0300

    alarm asset replaced

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -91,7 +91,7 @@ class _HomePageState extends State<HomePage> {
 }
 
 class AudioPlayerSettings {
-  static const String alarmSoundPath = "assets/audio/alarm2.mp3";
+  static const String alarmSoundPath = "assets/audio/alarm.wav";
 
   final AudioPlayer player;
 
@@ -125,7 +125,7 @@ class SensorMonitor {
   SensorMonitor(this.onSuspiciousMovement);
 
   void startMonitoring() {
-    const double movementThreshold = 30.0;
+    const double movementThreshold = 10.0;
     const int numberOfSamples = 5;
 
     List<double> accelerometerMagnitudes = [];
